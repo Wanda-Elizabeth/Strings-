c85efcd6b77153b9ff0c91188de09a1fdfb734fe
WandaElizabeth <wandawairimu@gmail.com>
2022-04-09 20:04:24 +0300

    Strings

---
+++ src/main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    val t="her name is"
+    val name="Rehema"
+    println(t +" "+ name)
+    //output
+    val  y="my name is"
+    val names="Wanjiru"
+    println(y + " "+ names)
+    //output
+    val r="we want that girl"
+    var word="Rehema"
+    println(r+ " " + word)
+    //output
+    val z="pawpaw"
+    println(z[0])
+    println(z[1])
+    println(z[2])
+    println(z[3])
+    println(z[4])
+    println(z[5])
+    val tt="mango"
+    println(tt[0])
+    println(tt[1])
+    println(tt[2])
+    println(tt[3])
+    println(tt[4])
+    val namess="khloe"
+    val year="2020"
+    val sentence="$namess was born in $year"
+    println(sentence)
+
+}
